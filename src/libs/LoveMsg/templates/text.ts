eab2dde75eedf24a9ae2fb92d3ec20d220bc7524
zht <zhanghaitao@jiazhengye.cn>
2022-02-08 19:00:33 +0800

    初版

--- src/libs/LoveMsg/templates/text.ts
+++ src/libs/LoveMsg/templates/text.ts
@@ -8,18 +8,18 @@ import dayjs, { weekToday } from '../../../utils/dayjs'
 export const textTemplate = (data: TextTemplateProps) => {
   const { caiHongpi, sayLove, songLyrics, oneMagazines, netEaseCloud, oneWord, dayEnglish } = data
 
-  let text = '早安呀，我可爱的鱼崽崽~\n'
+  let text = '早安呀，我可爱的包包~\n'
 
   // 工作日/休息日，需要排除节假日
   const week = weekToday()
-  if (['星期六', '星期日'].includes(week)) {
+  if (!['星期六', '星期日'].includes(week)) {
     text += `
-如果我鱼崽崽已经起床啦！崽崽向你说早安呦~，记得吃早饭呀😆\n
+如果我的包包已经起床啦！老公向你说早安呦~，记得吃早饭呀😆\n
 嗯哼哼~今天可是${week}哦，上班别迟到了哦~`
   }
   else {
     text += `
-如果我鱼崽崽还没起床呀！崽崽就等着鱼崽起床给我说早安呦🤣
+如果我的包包还没起床呀！老公就等着包包起床给我说早安呦🤣
 嗯哼~，既然今天是${week}，就让你再睡会懒觉~下次可不能啦~😝\n`
   }
 
